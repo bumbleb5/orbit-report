f8c2a599643a440190bcea25509a83a1e4a8b2c8
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2021-06-28 16:35:06 -0500

    sorting works

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -16,4 +16,28 @@ export class OrbitListComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    sort(column: keyof Satellite): void {
+        this.satellites.sort((a: Satellite, b: Satellite): number => {
+            if (a[column] < b[column]) {
+                return -1;
+            } else if (a[column] > b[column]) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
+    // sort(column: string): void {
+    //     // array.sort modifies the array, sorting the items based on the given compare function
+    //     this.satellites.sort(function(a: Satellite, b: Satellite): number {
+            
+    //         if(a[column] < b[column]) {
+    //             return -1;
+    //         } else if (a[column] > b[column]) {
+    //             return 1;
+    //         }
+    //         return 0;
+    //     });
+    //  }
+
 }
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,17 +1,25 @@
 export class Satellite {
-    
-    name:string;
-    type:string;
-    orbitType:string;
-    operational:boolean;
-    launchDate:string;
 
-    constructor(name:string, type:string, launchDate:string, orbitType:string, operational:boolean) {
+    name: string;
+    type: string;
+    orbitType: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
         this.type = type;
         this.launchDate = launchDate;
         this.orbitType = orbitType;
         this.operational = operational;
+    }
+
+    shouldShowWarning(): boolean {
+        if (this.type === 'Space Debris' || this.type.toLowerCase() === 'space debris') {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
